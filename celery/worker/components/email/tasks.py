ca41f2040578e4e356a0c4db163753e0780dc2c9
Petar Efnushev <petar.efnushev@keitaro.com>
2016-11-30 21:56:08 +0100

    Add task for sending emails

--- celery/worker/components/email/tasks.py
+++ celery/worker/components/email/tasks.py
@@ -1,5 +1,7 @@
 from worker import app
+from worker.utils import Mailer
 
 @app.task
-def send_mail():
-    pass
+def send_mail(recipient, subject, body):
+    mailer = Mailer(app.conf)
+    return mailer.send(recipient=recipient, subject=subject, body=body)
