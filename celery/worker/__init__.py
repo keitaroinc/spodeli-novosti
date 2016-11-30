553eff177d383d16f8e1dfef8ca3452863918c09
Petar Efnushev <petar.efnushev@keitaro.com>
2016-11-30 21:55:49 +0100

    Setup periodic tasks

--- celery/worker/__init__.py
+++ celery/worker/__init__.py
@@ -2,6 +2,7 @@
 import os
 
 from celery import Celery
+from datetime import timedelta
 
 # Load settings
 settings = os.getenv(
@@ -15,3 +16,12 @@ app.config_from_object(settings)
 # Register tasks
 app.autodiscover_tasks(['worker.components.test',
                         'worker.components.email'])
+
+# Register periodic tasks
+@app.on_after_configure.connect
+def setup_periodic_tasks(sender, **kwargs):
+    from .components.test.tasks import hello_world
+    sender.add_periodic_task(
+        timedelta(seconds=5),
+        hello_world.s(),
+    )
