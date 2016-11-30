f1e3e406fe0cd384efce27ce9c0b7a3a0881b1e7
Petar Efnushev <petar.efnushev@keitaro.com>
2016-11-30 17:15:10 +0100

    Remove news resource

--- api/app/__init__.py
+++ api/app/__init__.py
@@ -5,12 +5,9 @@ import settings
 # from .resources import *
 from .resources import IndexResource
 from .resources import SubscriberResource
-from .resources import NewsResource
 
 indexResource = IndexResource()
 subscriberResource = SubscriberResource()
-newsResource = NewsResource()
-
 
 def create_app():
     app = falcon.API()
--- api/app/resources/__init__.py
+++ api/app/resources/__init__.py
@@ -1,4 +1,3 @@
 # -*- coding:utf-8 -*-
 from .index import IndexResource
 from .subscriber import SubscriberResource
-from .news import NewsResource
