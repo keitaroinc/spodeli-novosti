4d7958c2be6645f67b5a291ad3a9dfeed9d812e9
Petar Efnushev <petar.efnushev@keitaro.com>
2016-11-30 21:57:07 +0100

    Add env vars for email operations

--- celery/worker/config.py
+++ celery/worker/config.py
@@ -9,6 +9,12 @@ class BaseConfig(object):
     CELERY_ENABLE_UTC = True
     CELERY_TIMEZONE = os.getenv('CELERY_TIMEZONE', 'UTC')
     CELERYD_CONCURRENCY = os.getenv('CELERYD_CONCURRENCY', 20)
+    SMTP_SERVER = os.getenv('SMTP_SERVER', None)
+    SMTP_SERVER_USER = os.getenv('SMTP_SERVER_USER', None)
+    SMTP_SERVER_PASSWORD = os.getenv('SMTP_SERVER_PASSWORD', None)
+    SMTP_SERVER_PORT = os.getenv('SMTP_SERVER_PORT', None)
+    FROM_EMAIL = os.getenv('FROM_EMAIL', 'info@keitaro.com')
+    FROM_NAME = os.getenv('FROM_NAME', 'root')
 
 
 class DevelopmentConfig(BaseConfig):
