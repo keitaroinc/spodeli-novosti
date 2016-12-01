219312b9ee1635f75c60afdff7249b7c8c529c91
Petar Efnushev <petar.efnushev@keitaro.com>
2016-12-01 18:53:20 +0100

    Add cors middleware

--- api/app/__init__.py
+++ api/app/__init__.py
@@ -7,16 +7,18 @@ from .resources import IndexResource
 from .resources import SubscriberResource
 from .resources import SendResource
 from .db import RethinkDBFactory
+from .middleware import CORSMiddleware
 
 indexResource = IndexResource()
 subscriberResource = SubscriberResource()
 sendResource = SendResource()
+corsMiddleware = CORSMiddleware()
 
 def create_app():
     db = RethinkDBFactory()
     db.create_table("subscribers")
 
-    app = falcon.API()
+    app = falcon.API(middleware=[corsMiddleware])
     app.add_route('/', indexResource)
     app.add_route('/subscriber', subscriberResource)
     app.add_route('/subscriber/{id}', subscriberResource)
